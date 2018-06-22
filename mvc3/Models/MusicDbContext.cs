6e6468f60da20086e1cc489ca5ebe876e6b4d13b
Antek <antonik68@gmail.com>
2018-06-22 23:21:03 +0200

    first commit

---
+++ mvc3/Models/MusicDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace mvc3.Models
+{
+    public class MusicDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+
+        public System.Data.Entity.DbSet<mvc3.Models.Genre> Genres { get; set; }
+    }
+}
---
+++ mvc3/Models/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvc3.Models
+{
+    public class Song
+    {
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Artist is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the artist of a song is 100 characters!")]
+        public string Artist { get; set; }
+        public int GenreId { get; set; }
+
+        [Key]
+        public int Id { get; set; }
+
+    }
+}
